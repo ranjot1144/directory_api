20e11544c33beb99c585ae28fe82f942654af4e5
Ranjot Singh <ranjyoth2007@gmail.com>
2023-10-19 12:27:04 +0100

    table changes

--- database/migrations/2023_10_16_092638_create_category_website_table.php
+++ database/migrations/2023_10_16_092638_create_category_website_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->unsignedBigInteger('website_id');
             $table->timestamps();
 
-            $table->foreign('category_id')->references('id')->on('categories');
+            //$table->foreign('category_id')->references('id')->on('categories');
             $table->foreign('website_id')->references('id')->on('websites');
         });
     }
